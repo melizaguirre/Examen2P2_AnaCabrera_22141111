21455a0e4ce96903914c8c64fe9b9b4875e64025
username <cabreraizaguirre2002@gmail.com>
2022-03-25 13:54:23 -0600

    Clase Cientifico

--- src/Cientifico.java
+++ src/Cientifico.java
@@ -1,3 +1,6 @@
+
+import java.util.ArrayList;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -9,5 +12,28 @@
  * @author BAC
  */
 public class Cientifico {
+    protected String nombreCient;
+    protected ArrayList<planeta> planetas = new ArrayList();
+
+    public Cientifico(String nombreCient) {
+        this.nombreCient = nombreCient;
+    }
+
+    public String getNombreCient() {
+        return nombreCient;
+    }
+
+    public void setNombreCient(String nombreCient) {
+        this.nombreCient = nombreCient;
+    }
+
+    public ArrayList<planeta> getPlanetas() {
+        return planetas;
+    }
+
+    public void setPlanetas(ArrayList<planeta> planetas) {
+        this.planetas = planetas;
+    }
+    
     
 }
